fa0f57e54865335ed7da5caad807b5541d3f4f21
shrungadithyaan <shrungadithyaan@gmail.com>
2025-07-11 04:18:12 +0530

    navbar commit 3

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between relative">
-        <Image src="/logo.png" alt="MockMate Logo" width={120} height={32} />
+         <Link href="https://intellihire-nine.vercel.app/"><Image src="/logo.png" alt="MockMate Logo" width={120} height={32} /> </Link>
 
         <div className="relative flex">
           <Button
